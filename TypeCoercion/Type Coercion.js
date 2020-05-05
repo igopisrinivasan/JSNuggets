90edec7901f49cfe3453fe35ff26775f13b12d05
Gopi Srinivasan <igopisrinivasan@gmail.comn>
2020-05-05 19:55:45 +0530

    Initial Commit

---
+++ TypeCoercion/Type Coercion.js
@@ -0,0 +1,8 @@
+const number = 123;
+console.log(typeof number); // number
+const numberToString = String(number);
+console.log(typeof numberToString); // string
+const object = {};
+console.log(Boolean(object)); // true
+const anotherObject = {name: 'I2i'};
+console.log(Boolean(anotherObject)); // true
