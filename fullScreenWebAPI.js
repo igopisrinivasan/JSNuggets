77adcd01c2b80bcea4d7f09dcd880733c4351112
Gopi Srinivasan <igopisrinivasan@gmail.com>
2020-07-01 19:25:33 +0530

    Fullscreen js added

---
+++ fullScreenWebAPI.js
@@ -0,0 +1,9 @@
+/* FullScreen mode can be achieved in a web page using  the API Provided on that HTML element.
+A  HTML element like an video can be viewed in fullscreen mode by using requestFullscreen() method.
+
+Fullscreen API works on standard HTML elements.
+
+It doesn't work on <dialog> element and elements inside iFrame  where Iframe doesn't have allowfullscreen attribute set
+*/
+
+// Request Fullscreen
