1d9d2564057201daf3dce3bbf602d2af5e03ddb7
Gopi Srinivasan <igopisrinivasan@gmail.com>
2020-07-02 14:05:41 +0530

    SWAPPING

---
+++ swapping.js
@@ -0,0 +1,29 @@
+/*
+  SWAPPING TWO 
+  WORDS OF A STRING 
+  IN JAVASCRIPT
+ 
+  HOW WOULD YOU SOLVE THIS?
+   
+   How to swap the first name and the last name in a 
+   string?
+    
+   For example, consider the name 'chandler Bing', can
+   you convert it into the following format using a 
+   single line of codein javascript?
+     
+   Bing,  Chandler.
+
+   THE SOLUTION 
+
+      Yes, we can use string replace() method to achieve this!
+      Each word in the string can be matched using the '\w+'
+      pattern matcher.
+
+      The regular expression will be:
+           '/(\w+)\s(
+
+
+
+
+*/
