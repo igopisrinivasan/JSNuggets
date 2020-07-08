a3a4f2db0ccacc5575378f9dadd212d6872001fe
Gopi Srinivasan <igopisrinivasan@gmail.com>
2020-07-08 12:47:40 +0530

    polymorphism

---
+++ Polumorphism.js
@@ -0,0 +1,26 @@
+/*
+
+POLYMORPHISM
+IN JAVASCRIPT
+
+POLYMORPHISM
+
+. Polymorphism is one of the principles of
+  Object-Oriented Programming (OOP)
+
+. It is used to share or override any behavior of 
+  objects with the specifications provided objects.
+
+. It takes the inheritance as a key to achieving
+  this
+  
+. Javascript is an interpreted language but not a 
+  compiled language. So method overloading is not
+  posisible  
+
+  .
+
+
+
+
+*/ 
